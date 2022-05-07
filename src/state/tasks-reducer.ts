35620b43aa3fddfd1fe319c9e5532d15cff004e8
Denis Shubin <denis.shubin@gmail.com>
2022-05-07 18:47:24 +0300

    feat: add app with redux

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -2,7 +2,9 @@ import {TasksStateType} from '../App';
 import {AddTodolistActionType, RemoveTodolistActionType} from './todolists-reducer';
 import {v1} from 'uuid';
 
-export const tasksReducer = (state: TasksStateType, action: ActionType): TasksStateType => {
+const initialState: TasksStateType = {}
+
+export const tasksReducer = (state: TasksStateType = initialState, action: ActionType): TasksStateType => {
     switch (action.type) {
         case 'REMOVE-TASK': {
             return {...state, [action.todolistId]: state[action.todolistId].filter(t => t.id !== action.taskId)}
@@ -44,7 +46,7 @@ export const tasksReducer = (state: TasksStateType, action: ActionType): TasksSt
             return stateCopy
         }
         default:
-            throw new Error('I don\'t understand this type')
+            return state
     }
 }
 
